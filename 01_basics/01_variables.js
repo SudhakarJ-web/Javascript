ed92af5c6c113ba8d8690e6d2d8188caf959cce5
Sudhakar Jadhav <35526465+SudhakarJ-web@users.noreply.github.com>
2024-02-14 15:47:15 +0000

    Variable in JS

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountID = 1705
+let accountName = "Shivray"
+var accountCity = "Pune"
+accountEmail = "Sudhakar@google.com"
+let accountState
+
+
+// accountID = 1710  //Cannot be changed due to const
+
+accountName = "Shivaji"
+accountCity = "Satara"
+accountEmail = "sudhakar@youtube.com"
+
+// console.log(accountID);
+
+console.table([accountID, accountCity, accountName, accountEmail,accountState])
+
+/*
+Dont prefer to use the var 
+because of block scope and functional scope
+*/

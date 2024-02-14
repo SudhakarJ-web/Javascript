1dfd11ff00a24c6d02be9e029f2fac3f4d5605f5
Sudhakar Jadhav <35526465+SudhakarJ-web@users.noreply.github.com>
2024-02-14 16:11:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // Treat all JS code as new version
+
+// alert (10+10) // Never run because we are using Nodejs NOT a browser
+
+// console.log("Sarvesh") console.log(3-4) //bad practise of coding because it kills readability of code
+
+
+let Name = "Sarvesh" //String Datatytpe
+
+let age = 29  //Number Datatype
+
+let loggedIn = false //Boolean Datatype
+
+let state;
+
+// Primitive datatypes 
+
+//Number  > number
+// BigInt > large number of data
+//String > --  prefer to use in ""
+// Boolean >--  True or False
+// Null >-- Standalone value
+//undefined > -- Varibale declared but value is not assigned
+//symbol >-- Used for uniqueness
+
+//Non Primitive datatypes
+//object
+
+
+console.log(typeof "Sid"); //string
+console.log(typeof undefined); //undefined 
+console.log(typeof null); // Object 

efc6f989fecce8319fc1a4a98c9c698b40a4b877
Sudhakar Jadhav <35526465+SudhakarJ-web@users.noreply.github.com>
2024-02-16 07:00:13 +0000

    Datatype conversion

---
+++ 01_basics/03_datatypeconversion.js
@@ -0,0 +1,28 @@
+let score = "Sandy"
+//console.log (score)
+//console.log (typeof score)
+
+let valuInNumber = Number (score)
+// console.log(typeof valuInNumber);
+// console.log(valuInNumber);
+
+// "33" //If number is in string it convert into number which 33
+// "33abcc" //Result NaN because string with number and character doesnt convert
+//true = 1 and false = 0
+
+let loggedIn = ""
+let booleanLoggedIn = Boolean(loggedIn)
+
+// console.log(booleanLoggedIn);
+
+// 1 >= true 
+// 0 >= false
+// "" > fasle
+// "Sandy" >= true
+
+let someNumber = 5034
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+// String Conversion
